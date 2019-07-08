2db40af2b753b5af9541dd9b1deb8448bdc1d614
greatorangejuice <bypavelsnigirev@gmail.com>
2019-07-08 17:42:35 +0300

    add email and password validate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    input.ng-invalid.ng-touched {
+      border: 1px solid red;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'CherryForms';
+  answers = [{
+    type: 'yes',
+    text: 'Да'
+  }, {
+    type: 'no',
+    text: 'Нет'
+  }];
+
+  submitForm(form: NgForm) {
+    console.log(form);
+  }
 }
